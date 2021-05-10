f1a86a827ec20a27263265c13fe6987c066684e7
Brenda Kiptim <kiptim54>
2021-05-10 23:43:14 +0300

    added 1 textfield

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const Navbar = () => {
     return (
-        <AppBar>
+        <AppBar position="sticky">
             <Toolbar>
                 <Typography variant="h6">Where in the world?</Typography>
             </Toolbar>
--- src/modules/main/App.tsx
+++ src/modules/main/App.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
+import { ThemeProvider, makeStyles, useTheme } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 //components
 import Navbar from 'src/components/navbar';
-
-const theme = createMuiTheme({
-    palette: {},
-});
+import { Grid } from '@material-ui/core';
+import Filters from 'src/modules/main/Filters';
 
 const App = (): JSX.Element => {
+    const customTheme = createMuiTheme({
+        spacing: 0,
+        palette: {},
+    });
+
+    const theme = useTheme();
+    console.log(theme);
+    const styles = makeStyles({
+        root: {},
+    });
+
+    const classes = styles();
+
     return (
-        <>
-            <Navbar />
-            <div>This is app.tsx</div>
-        </>
+        <CssBaseline>
+            <ThemeProvider theme={customTheme}>
+                <Grid container className={classes.root}>
+                    <Navbar />
+                    <Filters />
+                    <Grid container>
+                        <div>This is app.tsx</div>
+                    </Grid>
+                </Grid>
+            </ThemeProvider>
+        </CssBaseline>
     );
 };
 export default App;
