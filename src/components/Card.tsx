221f47e4708912db8307bb45cd1f1e9fc89d2d75
Brenda Kiptim <kiptim54>
2021-09-09 23:25:15 +0300

    added lazy loading to cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import Kenya from 'src/assets/images/kenya.png';
 import { ICountries } from 'src/redux/types';
+import LazyLoad from 'react-lazyload';
+import Skeleton from 'react-loading-skeleton';
+
 // DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 interface ICardsProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
     customClasses?: string;
     country: ICountries;
 }
 
+const LoadingSkeleton = () => {
+    return <Skeleton className="md:h-60 w-auto" />;
+};
 const Card = (props: ICardsProps): JSX.Element => {
     const { customClasses, country, ...rest } = props;
     return (
         <div className={`rounded-md shadow-lg bg-white h-full overflow-hidden ${customClasses}`} {...rest}>
             <div className=" w-full md:h-60  overflow-hidden">
-                <img style={{ objectFit: 'cover' }} className="w-full" src={country.flag} />
+                <LazyLoad placeholder={<LoadingSkeleton />} offset={-300} once={true}>
+                    <img style={{ objectFit: 'cover' }} className="w-full" src={country.flag} />
+                </LazyLoad>
             </div>
             <div className="p-4">
                 <h4 className="font-bold">{country.name}</h4>
